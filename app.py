a8c828433bbb535a093cdf06795c53b1149a7c4d
HCRICKET <heinhaung91@gmail.com>
2025-10-13 14:13:17 -0400

    PYTHON

---
+++ app.py
@@ -0,0 +1,29 @@
+# print ("Hello world")
+# age = 20
+#  age = 30
+# print(age)
+# name = input("what is your name")
+#  print ("Hello"+ name)
+
+# birth_year = input("what is your birth_year:")
+# age = 2025 - int(birth_year)
+# print(age)
+
+# int()
+# float()
+# bool()
+# str()
+
+# first = input("first:")
+# second = input ("second:")
+# sum = float(first)+float(second)
+# print("Sum:" + str(sum))
+
+# first = float(input("first:"))
+# second = float(input("second:"))
+# sum = first + second
+# print("Sum:"+ str (sum))
+
+course = 'Python for Beginners'
+print (course.find('y'))
+print(course)
