d53cc7a7b9a7ab8fd4a4c9dc25860b986508a0a3
HCRICKET <heinhaung91@gmail.com>
2025-10-14 00:25:16 -0400

    PYTHON

--- python.py
+++ python.py
@@ -1,1 +1,134 @@
-print ("Hello World")
+# print ("Hello World")
+# print("Hello New York City")
+# print(3*5)
+# print(input("what is your name:"))
+# my_name = input("what is your name:")
+# print("Hello"+ my_name)
+
+# first_num = int(input("enter your first number:"))
+# second_num= int(input("enter your second number:"))
+# print("The sum of your number is:"+ str(first_num + second_num))
+# age = 30
+# print(age)
+# first_name = "HCricket"
+# last_name  = "Aung"
+# full_name  = first_name + last_name
+# print (full_name)
+# x=5
+# y=7
+# z =x+y
+# print(z)
+# Data types number
+# int
+# x = 5 
+# y = -12
+# floats x=3.124
+
+
+# String 
+# name ="HCricket"
+# message= "Hello world"
+# full_name=name+"Hein"
+
+
+# Boolean 
+# is_student = True
+# is_enrolled = False
+# print(is_student and is_enrolled) #Output: True and False = False
+
+# Lists (collection of items and mutable which means you can change their content)
+# Lists = Arry in JS
+fruits =["apple","banana","orange"]
+# print(fruits[0])  #"apple"
+# fruits.append("grape") #append means add
+# print(fruits)
+
+# Tuple(lists-immutable(cannot change))
+# numbers = (1,2,3)
+# print(numbers[0])
+
+# Dictionaries
+person = { "name": "John","age":30}
+# print(person["name"])  #"John"
+
+#  add or modify
+# person["salary"]=2500.50
+# person["age"]=31
+# print(person)
+
+# Numeric operations in Python
+# a = 10
+# b = 3
+# addition +
+# subtraction -
+# multiplication(*)
+# division  a/b
+# modulo  a%b (remainder)
+# exponent a ** b (2*2*2)
+# c = 7
+# d = 2
+# floor_division c//d  #3
+# abosolute = abs (-10) #10
+# rounding= round(3.14159,2)  #rounding:3.14
+
+
+# Logical operation
+# AND,OR,not
+
+# if-else statement
+# number = 7
+# if number % 2 == 0:
+#  print ("The number is even")
+# else:
+#  print("The number is odd.")
+
+# score = 85
+# if score >= 90:
+#   print("Grade-A")
+# elif score >= 80:
+#   print("Grade-B")
+# elif score >=70:
+#   print("Grade-C")
+
+
+# temperature = float(input("Enter the temperature in Celsius:"))
+# if temperature < 0:
+#   print("It is freezing")
+# elif temperature>25:
+#   print("It is hot")
+# else:
+#   print("Temperature is pleasant")
+
+
+# username = input("Enter your username:")
+# password = input("enter your password:")
+# if username == "admin"  and password == "password":
+#   print("login successful!")
+# else: print("Invalid username or password")
+
+
+# Built-in data structures
+# List [order collection -commonly use][mutable][]
+my_list = [1,2,3,4,5]
+# print(my_list[-1]) #5
+# print(my_list[2])  #3
+# my_list[2]=10
+# my_list.append(6)
+# my_list.remove(4)
+# print(my_list)
+
+# tuple(immutable-cannot modified) (comma-separated values)()
+# my_tuple = (1,2,3,4,5)
+# print(my_tuple[-3])
+# print(my_tuple[1])
+
+
+# Sets(unorder collection/unique elements)without any duplicates {} or ()
+# set1 = {1,2,3}
+# set2 ={3,4,5}
+# print(set1.union(set2)) #{1,2,3,4,5}
+# print(set1.intersection(set2)) {3}
+# print(set1.difference(set2)) {1,2}
+# print(set2.difference(set1)) {4,5}
+
+
