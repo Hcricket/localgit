21f34aecd7af61a02b40dbd139e9103997bd3286
HCRICKET <heinhaung91@gmail.com>
2025-10-14 22:18:30 -0400

    User.py commit

--- User.py
+++ User.py
@@ -1,18 +1,22 @@
 class User:
- def __init__(self, name, email_address, drivers_license_number):
-    self.name = name
-    self.email_address = email_address
-    self.drivers_license_number = drivers_license_number
- 
- def __str__(self):
-    return f"User: {self.name}, email_address: {self.email_address}, drivers_license_number: {self.drivers_license_number}"
-
-
-  
-User1 = User(name="Cricket",email_address="heinhaung91@gmail.com",drivers_license_number=12345)
-User2 = User("SuperMan","Superduper@gmail.com",3456)
-print (User1)
+       def __init__ (self,name,email_address,driver_licence_number):
+         self.name = name 
+         self.email_address = email_address
+         self.driver_licence_number = driver_licence_number
+       def __str__(self):
+         return f"User:{self.name},Email:(self.email_address),Licence{self.driver_licence_number}" 
+User1 = User("HeinHtetAung","hein.123@gmail.com",1242342543)    
+User2 = User("CRICKEt","michaelpayit@gmail.com",234234132)       
+User3 = User("Phwarlay","Phwarlay@gmail.com",9843889)
+print(User1)
 print(User2)
+print(User3)
+
+
+
+
+
+
 
 # class User:
 #     def __init__(self, name, email_address, drivers_license_number):
